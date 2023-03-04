7ac3b092adff5d7dc2cc0a0730a4625e2092a9e8
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-03-04 15:00:11 -0600

    first go

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-let qwerty = document.getElementById('qwerty');
+let qwerty = document.querySelector('#qwerty');
 let phrase = document.getElementById('phrase');
 let missed = 0;
+let btn_reset = document.querySelector('.btn__reset');
+
+btn_reset.addEventListener('click', () => {
+    let overlay = document.getElementById('overlay');
+
+    overlay.style.display = "none";
+});
+
+console.log(qwerty);
+
+// qwerty.addEventListener('click', (e) => {
+//     if () {
+        
+//     } else {
+
+//     }
+// });
