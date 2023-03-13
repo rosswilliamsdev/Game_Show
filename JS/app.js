dfe83411ebec1ad4bf1837a84d0a9d2b6eefcf8e
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-03-13 18:05:09 -0500

    Removed all comments from app.js

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,9 @@
-// Get the element with the ID of qwerty and save it to a variable
 let qwerty = document.querySelector('#qwerty');
-// Get the element with the ID of phrase and save it to a variable.
 let phrase = document.getElementById('phrase');
-// Create a missed variable, initialized to 0 to keep track of the number of guesses the player has missed
 let missed = 0;
 let btn_reset = document.querySelector('.btn__reset');
 let overlay = document.getElementById('overlay');
-// Get all of the elements with a class of “letter”
 let letters = document.getElementsByClassName('letter');
-// Create a phrases array that contains at least 5 different phrases as strings
 let phrases = [
     "superficial commit",
     "burnout",
@@ -17,26 +12,17 @@ let phrases = [
     'marathon not a sprint'
 ];
 
-// Create a getRandomPhraseAsArray function
 function getRandomPhrases(arr) {
-    //choose a phrase from the phrases array
     let randomPhrase = phrases[Math.floor(Math.random()*phrases.length)];
-    //split that phrase into a new array of characters 
     randomPhrase = randomPhrase.split('');
-    //return the new array of characters
     return randomPhrase;
 };
 
-// Create an addPhraseToDisplay function that loops through an array of characters.
 function addPhraseToDisplay(arr) {
     for (let i = 0; i < arr.length; i++) {
-        // for each character in the array, you’ll create a list item
         let newLi = document.createElement('li');
-        // put the character inside of the list item
         newLi.textContent = arr[i];
-        // and append that list item to the #phrase ul in your HTML
         phrase.appendChild(newLi);
-        // If the character in the array is a letter and not a space, the function should add the class “letter” to the list item
         if (arr[i] !== " ") {
             newLi.className = "letter";
         };
@@ -46,33 +32,23 @@ function addPhraseToDisplay(arr) {
 let phraseArray = getRandomPhrases(phrases);
 addPhraseToDisplay(phraseArray);
 
-// The checkLetter function will be used inside of the event listener you’ll write in the next step.
-    // This function should have one parameter: the button the player has clicked when guessing a letter.
 function checkLetter(button) {
     let matchingLetter = null;
-    // The function should loop over the letters
     for (let i = 0; i < letters.length; i++) {
-        //and check if they match the letter in the button the player has chosen.
         if (button.textContent === letters[i].textContent) {
-            // store the matching letter inside of a variable
             matchingLetter = letters[i];
-            // If there’s a match, the function should add the “show” class to the list item containing that letter
             matchingLetter.classList.add('show');
         } 
     }
-    // and return that letter
     return matchingLetter;
 };
 
-// Attach an event listener to the “Start Game” button to hide the start screen overlay
 btn_reset.addEventListener('click', () => {
     let overlay = document.getElementById('overlay');
     overlay.style.display = "none";
 });
 
-// Each time the player guesses a letter, this function will check whether the game has been won or lost.
 function checkWin() {
-    // check if the number of letters with class “show” is equal to the number of letters with class “letters”
     let showClass = document.querySelectorAll('.show');
 
     if (showClass.length === letters.length) {
@@ -80,7 +56,6 @@ function checkWin() {
         overlay.className = 'win';
         overlay.innerHTML = `<h1>Congratulations!</h1><h3>You won the game!</h3>`;
     }
-    // if the number of misses is equal to or greater than 5, show the overlay screen with the “lose” class and appropriate text  
     else if (missed >= 5){
         overlay.style.display = 'block';
         overlay.className = 'lose';
@@ -89,26 +64,20 @@ function checkWin() {
 }
 
 
-// Use event delegation to listen only to button events from the keyboard
 qwerty.addEventListener('click', (e) => {
     let target = e.target;
-    // When a player chooses a letter, add the “chosen” class to that button so the same letter can’t be chosen twice. 
+    if (target.tagName === 'BUTTON') {
     target.classList.add('chosen');
-    // Note that button elements have an attribute you can set called “disabled” that when set to true will not respond to user clicks
     target.setAttribute('disabled', true);
-    // Pass the button to the checkLetter function, and store the letter returned inside of a variable called letterFound
     let letterFound = checkLetter(target);
     let oneTry = document.querySelector('.tries');
-    // After checkLetter is called, write a statement to check the value of the letterFound variable.
-    // If the value is null, remove one of the tries from the scoreboard.
+
     if (letterFound === null) {
-        // increase the missed count by 1
         missed++;
-        //Replace the heart with a lost heart
         oneTry.setAttribute('src', 'images/lostHeart.png');
-        // My idea to change the class name so oneTry querySelector always selects a different liveHeart after misses
         oneTry.classList.remove('tries');
         oneTry.classList.add('missed');
     }
+}
     checkWin();
 });
